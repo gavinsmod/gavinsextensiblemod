eeec7def89b8b8a0f68213897b0d87555b0b649c
Gavin Pease <gavinpease@gmail.com>
2022-05-22 11:13:30 -0600

    Add doc comment for ModNoClip
    Signed-off-by: Gavin Pease <gavinpease@gmail.com>

--- src/main/java/com/peasenet/mods/ModNoClip.java
+++ src/main/java/com/peasenet/mods/ModNoClip.java
@@ -3,6 +3,7 @@ package com.peasenet.mods;
 /**
  * @author gt3ch1
  * @version 5/22/2022
+ * A mod for allowing the printer to noclip (move through blocks)
  */
 public class ModNoClip extends Mod {
 
