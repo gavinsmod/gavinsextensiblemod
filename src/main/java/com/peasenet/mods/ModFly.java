62488d9e655a6a6d9f31e4dcfb8733fa3eed9dc0
Gavin Pease <gavinpease@gmail.com>
2022-05-24 18:32:28 -0600

    Don't enable nofall on enable of fly
    Signed-off-by: Gavin Pease <gavinpease@gmail.com>

--- src/main/java/com/peasenet/mods/ModFly.java
+++ src/main/java/com/peasenet/mods/ModFly.java
@@ -1,6 +1,5 @@
 package com.peasenet.mods;
 
-import com.peasenet.main.GavinsMod;
 import com.peasenet.util.KeyBindUtils;
 
 /**
@@ -10,12 +9,5 @@ import com.peasenet.util.KeyBindUtils;
 public class ModFly extends Mod {
     public ModFly() {
         super(Mods.FLY, Mods.Category.MOVEMENT, KeyBindUtils.registerKeyBindForType(Mods.FLY));
-    }
-
-    @Override
-    public void activate() {
-        super.activate();
-        if (!GavinsMod.NoFallEnabled())
-            GavinsMod.getModByType(Mods.NO_FALL).activate();
     }
 }
