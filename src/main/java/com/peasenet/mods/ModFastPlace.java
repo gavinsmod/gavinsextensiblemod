c13626c8bd0f2a8a025e2adfe1a4c2efdd6718f7
Gavin Pease <gavinpease@gmail.com>
2022-06-09 12:31:49 -0600

    Update some doc comments

--- src/main/java/com/peasenet/mods/ModFastPlace.java
+++ src/main/java/com/peasenet/mods/ModFastPlace.java
@@ -5,6 +5,11 @@ import com.peasenet.mixins.MinecraftClientAccessor;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.BlockItem;
 
+/**
+ * @author gt3ch1
+ * @version 6/9/2022
+ * A mod that allows the player to place blocks really fast.
+ */
 public class ModFastPlace extends Mod {
     public ModFastPlace() {
         super(Type.FAST_PLACE, Type.Category.MOVEMENT);
--- src/main/java/com/peasenet/mods/ModNoFall.java
+++ src/main/java/com/peasenet/mods/ModNoFall.java
@@ -3,6 +3,7 @@ package com.peasenet.mods;
 /**
  * @author gt3ch1
  * @version 5/23/2022
+ * A mod that prevents the player from taking fall damage.
  */
 public class ModNoFall extends Mod {
     public ModNoFall() {
