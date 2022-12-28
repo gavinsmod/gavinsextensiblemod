8fdc88b378fef7405f6fa7ddd8e19599c467cf46
GT3CH1 <gavinpease@gmail.com>
2022-12-28 13:50:57 -0700

    Increase size of ESP & Tracers GUI
    Signed-off-by: GT3CH1 <gavinpease@gmail.com>

--- src/main/java/com/peasenet/gui/mod/GuiTracers.java
+++ src/main/java/com/peasenet/gui/mod/GuiTracers.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2022. Gavin Pease and contributors.
+ * Copyright (c) 2022-2022. Gavin Pease and contributors.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction, including
  * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- *  of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
- *  following conditions:
+ * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
  *
  * The above copyright notice and this permission notice shall be included in all copies or substantial
  * portions of the Software.
@@ -37,7 +37,7 @@ public class GuiTracers extends GuiScroll {
      * Creates a new tracer dropdown.
      */
     public GuiTracers() {
-        this(new PointD(95, 120), 105, 10, Text.translatable("gavinsmod.gui.tracers"));
+        this(new PointD(100, 120), 110, 10, Text.translatable("gavinsmod.gui.tracers"));
     }
 
     /**
