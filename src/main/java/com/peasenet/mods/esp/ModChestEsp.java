d0691bbd87c7ae54ca28db8382954fcafe353466
GT3CH1 <gavinpease@gmail.com>
2022-12-28 13:42:53 -0700

    Fix chestmods rendering other blocks at inappropriate times
    Signed-off-by: GT3CH1 <gavinpease@gmail.com>

--- src/main/java/com/peasenet/mods/esp/ModChestEsp.java
+++ src/main/java/com/peasenet/mods/esp/ModChestEsp.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2022. Gavin Pease and contributors.
+ * Copyright (c) 2022-2022. Gavin Pease and contributors.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction, including
  * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- *  of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
- *  following conditions:
+ * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
  *
  * The above copyright notice and this permission notice shall be included in all copies or substantial
  * portions of the Software.
@@ -26,6 +26,9 @@ import com.peasenet.settings.ColorSetting;
 import com.peasenet.util.RenderUtils;
 import com.peasenet.util.event.data.BlockEntityRender;
 import com.peasenet.util.listeners.BlockEntityRenderListener;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.block.entity.EnderChestBlockEntity;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
 import net.minecraft.util.math.Box;
 
 /**
@@ -59,6 +62,8 @@ public class ModChestEsp extends Mod implements BlockEntityRenderListener {
     @Override
     public void onEntityRender(BlockEntityRender er) {
         var box = new Box(er.entity.getPos());
-        RenderUtils.drawBox(er.stack, er.buffer, box, espConfig.getChestColor());
+        if (er.entity instanceof ChestBlockEntity || er.entity instanceof ShulkerBoxBlockEntity
+                || er.entity instanceof EnderChestBlockEntity)
+            RenderUtils.drawBox(er.stack, er.buffer, box, espConfig.getChestColor());
     }
 }
