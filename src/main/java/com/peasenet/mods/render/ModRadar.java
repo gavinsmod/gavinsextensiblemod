7d967e933596ff9a0d92c947bd0154d392b01882
Gavin Pease <gavinpease@gmail.com>
2023-01-01 09:04:39 -0700

    Update copyright notice

--- src/main/java/com/peasenet/mods/render/ModRadar.java
+++ src/main/java/com/peasenet/mods/render/ModRadar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2022. Gavin Pease and contributors.
+ * Copyright (c) 2022-2023. Gavin Pease and contributors.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction, including
