259aa45c5deb644c2fd9dbb83dda7692c331e069
Gavin <gavinpease@gmail.com>
2023-07-21 11:14:37 -0600

    Update GEMS, update build script

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Gavin's Extensible Mod v1.0.0
 
-A successor to the non-extensible [gavinsmod](https://github.com/GT3CH1/minecraft-mod)
+A successor to the non-extensible [gavinsmod](https://github.com/gavinsmod/minecraft-mod) Minecraft client.
 
 ## Quick Terminology
 
@@ -19,7 +19,19 @@ This repository serves as a conglomerate of the following projects:
   projects that make up the GEM client. This repository is not meant to be used as a dependency
   nor is it meant to be used as a standalone mod. However, there is a helper script that will allow you
   to build all the GEMs and the core projects into their respective jars, as well as install them into
-  your Minecraft directory if wanted (see the `build-all.py` python script).
+  your Minecraft directory if wanted (see the [How do I build this
+  project?](#how-do-i-build-this-project) section)
+
+This repository will also be the repository to get up-to-date releases of the
+GEMs contained within this repository (for example: fullbright, xray, tracers),
+as well an up-to-date release of the gemclient-core library, which is required
+for all GEMs to work.
+
+This repository will _not_ host an up-to-date release of GavUI nor
+gavinsmod-events. You will need to get releases for those at the following
+locations:
+- GavUI: [https://github.com/GT3CH1/gavui](https://github.com/GT3CH1/gavui)
+- gavinsmod-events: [https://github.com/gavinsmod/gavinsmod-events](https://github.com/gavinsmod/gavinsmod-events)
 
 ## How can I help contribute to this project?
 
@@ -30,8 +42,33 @@ in this repository.
 
 ## How do I build this project?
 
-Please run the `build-all.py` python script to build all the GEMs and the core projects into their respective jars.
-Optionally, you may set COPY=1 in the environment variables to copy the jars into your Minecraft directory.
+Please run the `build.py` python script to build a portion or all of the core
+gavinsmod-client project. There are several options in this script that will
+help you with building this project.
+- `--clean`
+    - Whether to clean a given project before building. For example, `./build.py
+      --clean`
+    - Default: `False`
+- `--minecraft-dir` 
+    - The path to the directory of your Minecraft install. For example, `./build.py
+    --minecraft-dir=%APPDATA%\.minecraft\`
+    - Default: `~/.minecraft/`
+- `--copy`
+    - After a successful build, copy the jar file to your Minecraft directory.
+      For example, `./build.py --copy`
+    - Default: `False`
+- `--all`
+    - Builds __all__ projects contained into this repository. For example,
+      `./build.py --all`
+    - Default: `False`
+- `--directory`
+    - Builds only one of the projects under the given directory. For example,
+      `./build.py --directory mods/xray`
+    - Default: `None`
+
+As an example, if you want to build everything from scratch and copy to an alternative
+Minecraft installation, you may do so by calling: `./build.py --all --copy
+--minecraft-dir=/some/other/directory`
 
 ## How can I create my own GEM?
 
---
+++ build.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+import logging
+import os
+import subprocess
+import pathlib
+import re
+import sys
+import argparse
+
+logging.basicConfig(level=logging.DEBUG,
+                    format="[%(levelname)s] %(message)s")
+
+# Path to the directory containing this script
+SCRIPT_DIR = pathlib.Path(__file__).parent.absolute()
+
+parser = argparse.ArgumentParser (
+        prog='build.py',
+        description='Build everything that is part of the gavinsmod-client',
+        epilog='Made by @GT3CH1')
+
+parser.add_argument('--copy',required=False, action='store_true',
+                    help='Whether to copy freshly built GEMs to your Minecraft mods directory.')
+parser.add_argument('--clean',required=False, action='store_true',
+                    help="Whether to clean before buildling")
+parser.add_argument('--directory',required=False,
+                    help="A directory to a project")
+parser.add_argument('--minecraft-dir',required=False,default='~/.minecraft/',
+                    help="The directory to your Minecraft installation")
+parser.add_argument('--all',required=False, action='store_true',
+                    help="Whether to build all modules in this project.")
+args = parser.parse_args()
+print(args)
+CLEAN = args.clean
+MINECRAFT_DIR = args.minecraft_dir
+COPY = args.copy
+ALL = args.all
+DIRECTORY = args.directory
+
+
+
+
+def do_build(build_locs: []):
+    for mod_dir in build_locs:
+        if mod_dir.is_dir():
+            os.chdir(mod_dir)
+            name = mod_dir.name
+            if CLEAN:
+                logging.info("Cleaning %s",name)
+                res = os.system("gradle clean >/dev/null")
+                if res != 0:
+                    logging.error("Failed to clean %s", name)
+                    exit(1)
+            logging.info("Building %s", name)
+            res = os.system("gradle build >/dev/null")
+            if res != 0:
+                logging.error("Failed to build %s", name)
+                exit(1)
+            if COPY:
+                # get the right file by matching for gem-<file>-[0-9]*-[0-9]*-[0-9]*.jar file
+                jar_file = None
+                for file in mod_dir.joinpath("build/libs").iterdir():
+                    if re.match(r"gem-.*\-[0-9]*\.[0-9]*\.[0-9]*\.jar", file.name):
+                        jar_file = file
+                        break
+                if jar_file is None:
+                    # attempt to find a jar file at <name>-[0-9]*-[0-9]*-[0-9]*.jar
+                    for file in mod_dir.joinpath("build/libs").iterdir():
+                        if re.match(r".*\-[0-9]*\.[0-9]*\.[0-9]*\.jar", file.name):
+                            jar_file = file
+                            break
+                if jar_file is None:
+                    logging.error("Failed to find jar file for %s", name)
+                    exit(1)
+                logging.info("Copying %s to %s", jar_file.name, MINECRAFT_DIR)
+                res = os.system("cp %s %s/mods" % (jar_file, MINECRAFT_DIR))
+            
+if ALL:
+    logging.info("Building all!")
+    # check if "COPY" is set in the environment
+    build_locs = []
+    # add "gavui", "gemclient-core", and "gavinsmod-events" to the build list
+    build_locs.append(SCRIPT_DIR.joinpath("gavui"))
+    build_locs.append(SCRIPT_DIR.joinpath("gemclient-core"))
+    build_locs.append(SCRIPT_DIR.joinpath("gavinsmod-events"))
+
+    [build_locs.append(f) for f in SCRIPT_DIR.joinpath("mods").iterdir() if f.is_dir()]
+    do_build(build_locs)
+    exit(0)
+
+if DIRECTORY is not None:
+    build_locs = []
+    build_locs.append(SCRIPT_DIR.joinpath(DIRECTORY))
+    do_build(build_locs)
+    exit(0)
+
+parser.print_help(sys.stderr)
