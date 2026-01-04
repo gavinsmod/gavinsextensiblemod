11b962701d919226494d51aa004e07cfa48d2f1f
gt3ch1 <gcpease@peasenet.com>
2026-01-04 09:53:30 -0700

    [Xray] fix radar not respecting liquids config

--- src/main/java/com/peasenet/mixins/MixinLiquidBlockRenderer.java
+++ src/main/java/com/peasenet/mixins/MixinLiquidBlockRenderer.java
@@ -1,15 +1,21 @@
 package com.peasenet.mixins;
 
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.peasenet.config.render.XrayConfig;
 import com.peasenet.main.Mods;
 import com.peasenet.main.Settings;
 import com.peasenet.util.ChatCommand;
 import net.minecraft.client.renderer.block.LiquidBlockRenderer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
@@ -25,9 +31,29 @@ public class MixinLiquidBlockRenderer {
     private static void isFaceOccluded(CallbackInfoReturnable<Boolean> cir) {
         if (Mods.isActive(ChatCommand.Xray)) {
             XrayConfig xrayConfig = Settings.INSTANCE.getConfig(ChatCommand.Xray);
-            if(xrayConfig.getShowLiquids())
-                return;
-            cir.setReturnValue(false);
+            if (!xrayConfig.getShowLiquids()) {
+                cir.setReturnValue(false);
+            }
         }
+    }
+
+    @Inject(at = @At("HEAD"), method = "tesselate", cancellable = true)
+    void test(BlockAndTintGetter blockAndTintGetter, BlockPos blockPos, VertexConsumer vertexConsumer, BlockState blockState, FluidState fluidState, CallbackInfo ci) {
+        if (Mods.isActive(ChatCommand.Xray)) {
+            XrayConfig xrayConfig = Settings.INSTANCE.getConfig(ChatCommand.Xray);
+            if (!xrayConfig.getShowLiquids()) {
+                ci.cancel();
+            }
+        }
+    }
+
+    @ModifyConstant(method = "vertex", constant = @Constant(floatValue = 1.0f, ordinal = 0))
+    private static float modifyAlpha(float original) {
+        if (Mods.isActive(ChatCommand.Xray)) {
+            XrayConfig xrayConfig = Settings.INSTANCE.getConfig(ChatCommand.Xray);
+            if (!xrayConfig.getShowLiquids())
+                return 0f;
+        }
+        return original;
     }
 }
